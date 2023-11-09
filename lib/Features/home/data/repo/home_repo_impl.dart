2946bb25638fc9611062d680c9bcaf58c7ebdc01
karar0120 <92108624+karar0120@users.noreply.github.com>
2023-11-09 08:46:20 +0200

    create server failure error dio

--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/Features/home/data/repo/home_repo.dart';
 import 'package:bookly/core/error/failure.dart';
 import 'package:bookly/core/utils/api_services.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   ApiService apiService;
@@ -19,7 +20,10 @@ class HomeRepoImpl implements HomeRepo {
       return right(List<BookModel>.from(
           (data['items'] as List).map((e) => BookModel.fromJson(e))));
     }catch (e) {
-      return left(ServerFailure());
+      if (e is DioException){
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,9 +1,53 @@
+import 'package:dio/dio.dart';
+
 abstract class Failure {
-  // final String errMessage;
-  //
-  // const Failure(this.errMessage);
+  final String errMessage;
+
+  const Failure({required this.errMessage});
 }
 
 class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+
+
+  factory ServerFailure.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errMessage: 'Connection timeout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errMessage:'Send timeout with ApiServer');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errMessage:'Receive timeout with ApiServer');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(errMessage:'Request to ApiServer was canceled');
+
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure(errMessage:'No Internet Connection');
+        }
+        return ServerFailure(errMessage:'Unexpected Error, Please try again!');
+      default:
+        return ServerFailure(errMessage:'Opps There was an Error, Please try again');
+    }
+  }
+
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errMessage: response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure(errMessage:'Your request not found, Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure(errMessage:'Internal Server error, Please try later');
+    } else {
+      return ServerFailure(errMessage:'Opps There was an Error, Please try again');
+    }
+  }
 
 }
